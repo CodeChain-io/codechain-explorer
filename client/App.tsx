17454d59b6b4578c6eee9a17640dc3c416a36d09
JinGyeong Jeong <jjg@kodebox.io>
2018-05-08 15:28:41 +0900

    Move directory from src/ to client/

--- client/App.tsx
+++ client/App.tsx
@@ -12,7 +12,7 @@ class App extends React.Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          To get started, edit <code>client/App.tsx</code> and save to reload.
         </p>
       </div>
     );
