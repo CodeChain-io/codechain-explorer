36b79ed656a6db7efbf54e93d869f65a397f4922
JinGyeong Jeong <jjg@kodebox.io>
2018-05-09 19:14:31 +0900

    Add redux connect to ServerStatus with ping

--- client/ServerStatus.tsx
+++ client/ServerStatus.tsx
@@ -1,14 +1,51 @@
 import * as React from "react";
+import { connect, Dispatch } from "react-redux";
 
-class ServerStatus extends React.Component {
+import { IRootState } from "./redux/reducers";
+
+interface IServerStatusProps {
+    isNodeAlive: boolean | null;
+}
+
+// FIXME:
+interface IDispatcherProps {
+    ping: any
+}
+
+class ServerStatusInternal extends React.Component<IServerStatusProps & IDispatcherProps> {
+    public componentDidMount() {
+        setInterval(() => {
+            this.props.ping();
+        }, 3000);
+    }
+
     public render() {
-        // FIXME:
-        return (
-            <div>
-                Server is not available!
-            </div>
-        )
+        const { isNodeAlive } = this.props;
+        if (isNodeAlive === null) {
+            return <div>Checking whether node is alive</div>
+        } else if (isNodeAlive) {
+            return <div>Node is available</div>
+        } else {
+            return <div>Node is not available</div>
+        }
     }
 }
+
+const mapStateToProps = (state: IRootState) => ({ isNodeAlive: state.isNodeAlive });
+const mapDispatchToProps = (dispatch: Dispatch) => {
+    return {
+        ping: async () => {
+            fetch("http://localhost:8081/api/ping").then(res => {
+                return res.text()
+            }).then(text => {
+                dispatch({ type: "PING", payload: text });
+            }).catch(err => {
+                dispatch({ type: "PING", payload: null });
+            });
+        }
+    } as IDispatcherProps;
+};
+
+const ServerStatus = connect(mapStateToProps, mapDispatchToProps)(ServerStatusInternal);
 
 export default ServerStatus;
