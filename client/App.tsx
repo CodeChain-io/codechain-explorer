5bbf7a8f5084dde8ed914dad0249b88eb3e59f08
JinGyeong Jeong <jjg@kodebox.io>
2018-05-09 16:32:25 +0900

    Add mock ServerStatus component

--- client/App.tsx
+++ client/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 
 import logo from './logo.svg';
 
+import ServerStatus from "./ServerStatus";
+
 class App extends React.Component {
   public render() {
     return (
@@ -14,6 +16,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>client/App.tsx</code> and save to reload.
         </p>
+        <ServerStatus />
       </div>
     );
   }
---
+++ client/ServerStatus.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+class ServerStatus extends React.Component {
+    public render() {
+        // FIXME:
+        return (
+            <div>
+                Server is not available!
+            </div>
+        )
+    }
+}
+
+export default ServerStatus;
