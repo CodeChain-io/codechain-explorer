216e242e24d0d3a18ce325ffdb5fe42b1f0c3350
JinGyeong Jeong <jjg@kodebox.io>
2018-05-10 16:30:29 +0900

    Add apis

--- server/api.ts
+++ server/api.ts
@@ -1,6 +1,6 @@
-import * as express from "express";
-import * as cors from "cors";
-import { ServerContext } from "./context";
+import * as cors from 'cors';
+import * as express from 'express';
+import { ServerContext } from './context';
 
 const corsOptions = {
     origin: true,
@@ -21,6 +21,28 @@ export function createApiRouter(context: ServerContext, useCors = false) {
         const { codechainSdk } = context;
         codechainSdk.ping().then(text => {
             res.send(JSON.stringify(text));
+        }).catch(next);
+    });
+
+    router.get("/blockNumber", async (req, res, next) => {
+        context.codechainSdk.getBlockNumber().then(text => {
+            res.send(JSON.stringify(text));
+        }).catch(next);
+    });
+
+    router.get("/block/:blockNumber", async (req, res, next) => {
+        const { blockNumber } = req.params;
+        context.codechainSdk.getBlockHash(Number.parseInt(blockNumber)).then(hash => {
+            return context.codechainSdk.getBlock(hash);
+        }).then(block => {
+            res.send(JSON.stringify(block));
+        }).catch(next);
+    });
+
+    router.get("/tx/:txhash/invoice", async (req, res, next) => {
+        const { txhash } = req.params;
+        context.codechainSdk.getTransactionInvoice(txhash, 0).then(invoice => {
+            res.send(JSON.stringify(invoice));
         }).catch(next);
     });
 
