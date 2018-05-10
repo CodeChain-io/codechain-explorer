a3850a9181f71e20673c0d059c64e4d090b937bd
JinGyeong Jeong <jjg@kodebox.io>
2018-05-10 18:41:47 +0900

    Introduce router

--- client/index.tsx
+++ client/index.tsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+
 import App from './App';
 import './index.css';
 
 import { store } from "./redux/store";
 import registerServiceWorker from './registerServiceWorker';
 
+const Status = () => <div>Status</div>;
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/status">Status</Link></li>
+        </ul>
+        <Route exact={true} path="/" component={App} />
+        <Route path="/status" component={Status} />
+      </div>
+    </Router>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
