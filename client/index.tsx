f61d2b7d94f8ee66317d244df55792df05489542
JinGyeong Jeong <jjg@kodebox.io>
2018-05-10 20:29:12 +0900

    Add Home page add ServerStatus to the header

--- client/index.tsx
+++ client/index.tsx
@@ -3,12 +3,16 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
-import App from './App';
 import './index.css';
 
 import { store } from "./redux/store";
 import registerServiceWorker from './registerServiceWorker';
+import Block from './pages/Block';
+import ServerStatus from './ServerStatus';
+import { RequestPing } from './components/requests';
+import Home from './pages/Home';
 
+// FIXME:
 const Status = () => <div>Status</div>;
 
 ReactDOM.render(
@@ -19,8 +23,12 @@ ReactDOM.render(
           <li><Link to="/">Home</Link></li>
           <li><Link to="/status">Status</Link></li>
         </ul>
-        <Route exact={true} path="/" component={App} />
+        <ServerStatus />
+        <RequestPing />
+        <hr/>
+        <Route exact={true} path="/" component={Home} />
         <Route path="/status" component={Status} />
+        <Route path="/block/:id" component={Block} />
       </div>
     </Router>
   </Provider>,
---
+++ client/pages/Home.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import * as _ from "lodash";
+import { connect } from 'react-redux';
+import { RootState } from '../redux/actions';
+import { RequestBlockNumber } from '../components/requests';
+import { Link } from 'react-router-dom';
+
+interface StateProps {
+    bestBlockNumber?: number;
+}
+
+class HomeInternal extends React.Component<StateProps> {
+    public render() {
+        const { bestBlockNumber } = this.props;
+        if (bestBlockNumber === undefined) {
+            return (
+                <div>
+                    Loading ...
+                    <RequestBlockNumber />
+                </div>
+            );
+        }
+        return (
+            <div>
+                <div>Current Block Number: {bestBlockNumber}</div>
+                {_.map(_.range(1, bestBlockNumber + 1), n => (
+                    <div key={`home-block-num-${n}`}>
+                        <Link to={`/block/${n}`}>{n}</Link>
+                    </div>
+                ))};
+            </div>
+        );
+    }
+}
+
+const Home = connect((state: RootState) => {
+    return {
+        bestBlockNumber: state.bestBlockNumber
+    } as StateProps;
+})(HomeInternal);
+
+export default Home;
