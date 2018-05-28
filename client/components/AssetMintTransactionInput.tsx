2446df18f0c5da34c9609e735a6726e14540daa4
JinGyeong Jeong <jjg@kodebox.io>
2018-05-28 21:07:48 +0900

    Support AssetMint for TransactionInput

---
+++ client/components/AssetMintTransactionInput.tsx
@@ -0,0 +1,95 @@
+import * as React from "react";
+
+import { H160, H256 } from "codechain-sdk/lib/primitives";
+import { AssetMintTransaction } from "codechain-sdk/lib/primitives/transaction";
+
+interface Props {
+    onChange: (t: AssetMintTransaction) => void;
+}
+
+interface State {
+    nonce: number;
+    metadata: string;
+    lockScriptHash: string;
+    parameters: string[];
+    amount: number;
+    registrar: string;
+}
+
+export default class AssetMintTransactionInput extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            // FIXME: currently nonce is not used for AssetMint.
+            nonce: 0,
+            metadata: "",
+            lockScriptHash: "0000000000000000000000000000000000000000000000000000000000000000",
+            parameters: [],
+            amount: 100,
+            registrar: "",
+        }
+        this.emitChange();
+    }
+
+    public componentDidUpdate(_: Props, prevState: State) {
+        if (prevState !== this.state) {
+            this.emitChange();
+        }
+    }
+
+    public render() {
+        const { metadata, lockScriptHash, amount, registrar } = this.state;
+        return <div>
+            <span>Metadata</span>
+            <input onChange={this.onChangeMetadata} value={metadata} />
+            <span>Lock Script Hash</span>
+            <input onChange={this.onChangeLockScriptHash} value={lockScriptHash} />
+            <span>Amount</span>
+            <input onChange={this.onChangeAmount} value={amount} />
+            <span>Registrar</span>
+            <input onChange={this.onChangeRegistrar} value={registrar} />
+        </div>;
+    }
+
+    private onChangeMetadata = (event: any) => {
+        this.setState({
+            ...this.state,
+            metadata: event.target.value
+        });
+    }
+
+    private onChangeLockScriptHash = (event: any) => {
+        this.setState({
+            ...this.state,
+            lockScriptHash: event.target.value
+        });
+    }
+
+    private onChangeAmount = (event: any) => {
+        this.setState({
+            ...this.state,
+            amount: event.target.value
+        });
+    }
+
+    private onChangeRegistrar = (event: any) => {
+        this.setState({
+            ...this.state,
+            registrar: event.target.value
+        });
+    }
+
+    // FIXME: throttle
+    private emitChange = () => {
+        const { metadata, lockScriptHash, amount, registrar } = this.state;
+        this.props.onChange(new AssetMintTransaction({
+            metadata,
+            lockScriptHash: new H256(lockScriptHash),
+            // FIXME:
+            parameters: [],
+            // FIXME: U256
+            amount,
+            registrar: registrar.length === 40 ? new H160(registrar) : null,
+        }))
+    }
+}
--- client/components/TransactionInput.tsx
+++ client/components/TransactionInput.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { Transaction } from "codechain-sdk/lib";
 import PaymentTransactionInput from "./PaymentTransactionInput";
-
+import AssetMintTransactionInput from "./AssetMintTransactionInput";
 
 interface Props {
     type: "payment" | "setRegularKey" | "assetMint" | "assetTransfer";
@@ -16,6 +16,10 @@ export default class TransactionInput extends React.Component<Props> {
         if (type === "payment") {
             return <PaymentTransactionInput
                 nonce={nonce}
+                onChange={this.props.onChangeTransaction} />
+        }
+        if (type === "assetMint") {
+            return <AssetMintTransactionInput
                 onChange={this.props.onChangeTransaction} />
         }
         return <div>Not implemented for type: {type}</div>
