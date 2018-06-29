a069e423c653f2645a273ba7f4215544554d237a
Gilyoung Kim <roadzero@kodebox.io>
2018-06-29 17:17:26 +0900

    Move some element to latestBlocks

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import * as _ from "lodash";
 import { RequestBlockNumber, RequestBlock } from '../request';
-import { Link } from 'react-router-dom';
 import { Container } from 'reactstrap';
 import PendingParcelList from '../components/parcel/PendingParcelList';
 import { Block } from 'codechain-sdk';
+import LatestBlocks from '../components/home/LatestBlocks';
 
 interface State {
     bestBlockNumber?: number;
@@ -39,26 +39,10 @@ class Home extends React.Component<{}, State> {
         return (
             <div>
                 <Container>
-                    <div>Current Block Number: {bestBlockNumber}</div>
+                    <LatestBlocks blocksByNumber={blocksByNumber} />
                     {_.map(_.reverse(_.range(0, bestBlockNumber + 1)), n => {
-                        return (
-                            <div key={`home-block-num-${n}`}>
-                                <hr />
-                                <h3><Link to={`/block/${n}`}>Block {n}</Link></h3>
-                                {blocksByNumber[n]
-                                    ? (
-                                        <div>
-                                            <div>Hash: {blocksByNumber[n].hash.value}</div>
-                                            <div>Author: {blocksByNumber[n].author.value}</div>
-                                            <div>Total {blocksByNumber[n].parcels.length} Parcels</div>
-                                        </div>
-                                    )
-                                    : <RequestBlock id={n} onBlock={this.onBlock} onError={this.onError} />
-                                }
-                            </div>
-                        );
+                        return <RequestBlock id={n} onBlock={this.onBlock} onError={this.onError} />
                     })}
-                    <hr />
                     <PendingParcelList />
                 </Container>
             </div>
