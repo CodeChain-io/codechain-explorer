7ccc612fcc42b1d0540127e348b1ec8a610fd5ea
Gilyoung Kim <roadzero@kodebox.io>
2018-07-05 18:59:34 +0900

    Add more info to latest transactions in home

--- client/components/home/LatestTransactions/LatestTransactions.tsx
+++ client/components/home/LatestTransactions/LatestTransactions.tsx
@@ -4,7 +4,7 @@ import * as moment from "moment";
 import { Table } from 'reactstrap';
 import { Link } from "react-router-dom";
 
-import { Block } from "codechain-sdk";
+import { Block, ChangeShardState, AssetMintTransaction } from "codechain-sdk";
 
 import './LatestTransactions.css';
 
@@ -32,14 +32,21 @@ const LatestTransactions = (props: Props) => {
                     {
                         _.map(_.reverse(_.values(blocksByNumber)), block => {
                             return _.map(block.parcels, (parcel) => {
-                                return (
-                                    <tr key={`home-transaction-hash-${block.hash.value}`}>
-                                        <th scope="row"><Link to={`/block/${block.number}`}>{block.number}</Link></th>
-                                        <td>{block.parcels.length}</td>
-                                        <td>{}</td>
-                                        <td>{moment.unix(block.timestamp).fromNow()}</td>
-                                    </tr>
-                                );
+                                if (parcel.unsigned.action instanceof ChangeShardState) {
+                                    const transactions = parcel.unsigned.action.transactions;
+                                    return _.map(transactions, (transaction) => {
+                                        return (
+                                            <tr key={`home-transaction-hash-${transaction.hash().value}`}>
+                                                <th scope="row"><Link to="#">{transaction.hash().value}</Link></th>
+                                                <td>{parcel.unsigned.action.toJSON().action}</td>
+                                                {/* TODO : Show Details */}
+                                                <td>{transaction instanceof AssetMintTransaction ? 'Asset mint' : 'Asset transfer'}</td>
+                                                <td>{moment.unix(block.timestamp).fromNow()}</td>
+                                            </tr>
+                                        );
+                                    });
+                                }
+                                return null;
                             })
                         })
                     }
