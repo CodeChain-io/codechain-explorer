3769ce0c45a244d2640050e63e2204ac3677f29d
Gilyoung Kim <roadzero@kodebox.io>
2018-07-09 18:29:32 +0900

    Remove warning in production build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -296,6 +296,8 @@ module.exports = {
         ],
     },
     plugins: [
+        // For removing "Critical Dependencies" Warning.
+        new webpack.IgnorePlugin(/\/iconv-loader$/),
         // Makes some environment variables available in index.html.
         // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
         // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
