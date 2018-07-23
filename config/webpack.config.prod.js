dabbeb1d80bdaa184d16f379c3026f516bfea274
Gilyoung Kim <roadzero@kodebox.io>
2018-07-23 19:02:00 +0900

    Fix lint error

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -284,7 +284,7 @@ module.exports = {
                                 publicPath: '../' // override the default path
                             }
                         }]
-                    }
+                    },
                     // "file" loader makes sure assets end up in the `build` folder.
                     // When you `import` an asset, you get its filename.
                     // This loader doesn't use a "test" so it will catch all modules
