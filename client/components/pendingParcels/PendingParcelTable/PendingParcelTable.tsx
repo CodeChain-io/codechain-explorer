29a58655fce3dcc2a2f760d3fa99f6dada88d558
Gilyoung Kim <roadzero@kodebox.io>
2018-07-23 17:19:18 +0900

    Implement pagination in pending parcel

--- client/components/pendingParcels/PendingParcelTable/PendingParcelTable.tsx
+++ client/components/pendingParcels/PendingParcelTable/PendingParcelTable.tsx
@@ -11,25 +11,38 @@ interface Prop {
     pendingParcels: PendingParcelDoc[];
 }
 
-class PendingParcelTable extends React.Component<Prop, {}> {
+interface State {
+    itemPerPage: number,
+    currentPage: number
+}
+
+class PendingParcelTable extends React.Component<Prop, State> {
     constructor(props: Prop) {
         super(props);
+        this.state = {
+            currentPage: 1,
+            itemPerPage: 5
+        };
     }
 
     public render() {
         const { pendingParcels } = this.props;
+        const { currentPage, itemPerPage } = this.state;
+        const filteredParcel = pendingParcels;
+        const sortedParcels = filteredParcel;
+        const maxPage = Math.floor(filteredParcel.length / itemPerPage) + 1;
         return (
             <div className="pending-parcel-table">
                 <div>
                     <div>
                         <div className="float-right">
                             <span>Show </span>
-                            <select>
+                            <select onChange={this.handleOptionChange} defaultValue={itemPerPage.toString()}>
+                                <option value="5">5</option>
                                 <option value="10">10</option>
-                                <option value="20">20</option>
-                                <option value="30">30</option>
-                                <option value="40">40</option>
+                                <option value="25">25</option>
                                 <option value="50">50</option>
+                                <option value="100">100</option>
                             </select>
                             <span> entities</span>
                         </div>
@@ -48,9 +61,9 @@ class PendingParcelTable extends React.Component<Prop, {}> {
                             </thead>
                             <tbody>
                                 {
-                                    _.map(pendingParcels, pendingParcel => {
+                                    _.map(sortedParcels.slice((currentPage - 1) * itemPerPage, (currentPage - 1) * itemPerPage + itemPerPage), (pendingParcel, index) => {
                                         return (
-                                            <tr key={`pending-parcel-${pendingParcel.parcel.hash}`}>
+                                            <tr key={`pending-parcel-${pendingParcel.parcel.hash}-${index}`}>
                                                 <td><span className={`type-circle ${Type.isChangeShardStateDoc(pendingParcel.parcel.action) ? "change-shard-state-type" : (Type.isPaymentDoc(pendingParcel.parcel.action) ? "payment-type" : "set-regular-key-type")}`}>&nbsp;</span><HexString link={`/parcel/0x${pendingParcel.parcel.hash}`} text={pendingParcel.parcel.hash} /></td>
                                                 <td><HexString link={`/addr-platform/0x${pendingParcel.parcel.sender}`} text={pendingParcel.parcel.sender} /></td>
                                                 <td>{pendingParcel.parcel.fee}</td>
@@ -70,19 +83,19 @@ class PendingParcelTable extends React.Component<Prop, {}> {
                         </div>
                         <div className="d-inline">
                             <ul className="list-inline">
-                                <li className="list-inline-item page-btn">
+                                <li className={`list-inline-item page-btn ${currentPage === 1 ? "disable" : ""}`} onClick={this.moveFirst}>
                                     &lt;&lt;
                                 </li>
-                                <li className="list-inline-item page-btn">
+                                <li className={`list-inline-item page-btn ${currentPage === 1 ? "disable" : ""}`} onClick={this.moveBefore}>
                                     &lt; Prev
                                 </li>
                                 <li className="list-inline-item">
-                                    1 of 1
+                                    {currentPage} of {maxPage}
                                 </li>
-                                <li className="list-inline-item page-btn">
+                                <li className={`list-inline-item page-btn ${currentPage === maxPage ? "disable" : ""}`} onClick={_.partial(this.moveNext, maxPage)}>
                                     Next &gt;
                                 </li>
-                                <li className="list-inline-item page-btn">
+                                <li className={`list-inline-item page-btn ${currentPage === maxPage ? "disable" : ""}`} onClick={_.partial(this.moveLast, maxPage)}>
                                     &gt;&gt;
                                 </li>
                             </ul>
@@ -91,6 +104,42 @@ class PendingParcelTable extends React.Component<Prop, {}> {
                 </div>
             </div>
         )
+    }
+
+    private moveNext = (maxPage: number, e: any) => {
+        e.preventDefault();
+        if (this.state.currentPage >= maxPage) {
+            return;
+        }
+        this.setState({ currentPage: this.state.currentPage + 1 })
+    }
+
+    private moveLast = (maxPage: number, e: any) => {
+        e.preventDefault();
+        if (this.state.currentPage >= maxPage) {
+            return;
+        }
+        this.setState({ currentPage: maxPage })
+    }
+
+    private moveBefore = (e: any) => {
+        e.preventDefault();
+        if (this.state.currentPage <= 1) {
+            return;
+        }
+        this.setState({ currentPage: this.state.currentPage - 1 })
+    }
+
+    private moveFirst = (e: any) => {
+        if (this.state.currentPage <= 1) {
+            return;
+        }
+        this.setState({ currentPage: 1 })
+    }
+
+    private handleOptionChange = (event: any) => {
+        const selected = event.target.value;
+        this.setState({ itemPerPage: selected, currentPage: 1 });
     }
 }
 
