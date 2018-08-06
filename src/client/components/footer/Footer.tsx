66d6841132fb8437369fccf85e8c014d7929ce11
Gilyoung Kim <roadzero@kodebox.io>
2018-08-06 14:03:02 +0900

    Modify the styles of the footer

--- src/client/components/footer/Footer.tsx
+++ src/client/components/footer/Footer.tsx
@@ -1,14 +1,34 @@
 import * as React from "react";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
+import * as FontAwesome from "react-fontawesome";
 import "./Footer.scss";
+import * as Logo from "./img/logo.png";
+import * as GitterIcon from "./img/gitter.png";
 
 const Footer = () => {
     return <div className="footer">
         <Container>
-            <div className="text-center">
-                <p>codechain@kodebox.io</p>
-                <p className="mb-0">ⓒ 2018 Kodebox, Inc.</p>
-            </div>
+            <Row>
+                <Col lg="4">
+                    <img src={Logo} className="logo" />
+                    <p className="copyright">&copy; 2018 Kodebox,Inc. All Rights Reserved.</p>
+                </Col>
+                <Col lg="5">
+                    <ul className="mb-3 mb-lg-0 home-link-list">
+                        <li><a href="https://codechain.io" target="_blank">codechain.io</a></li>
+                        <li><a href="https://kodebox.io" target="_blank">kodebox.io</a></li>
+                        <li>codechain@kodebox.io</li>
+                    </ul>
+                </Col>
+                <Col lg="3">
+                    <ul className="list-inline link-list">
+                        <li className="list-inline-item"><a href="https://github.com/CodeChain-io/codechain" target="_blank"><FontAwesome name="github" /></a></li>
+                        <li className="list-inline-item"><a href="https://gitter.im/CodeChain-io/codechain" target="_blank"><img src={GitterIcon} className="gitter-icon" /></a></li>
+                        <li className="list-inline-item"><a href="https://twitter.com/codechain_io" target="_blank"><FontAwesome name="twitter" /></a></li>
+                        <li className="list-inline-item"><a href="https://www.facebook.com/codechain/" target="_blank"><FontAwesome name="facebook-square" /></a></li>
+                    </ul>
+                </Col>
+            </Row>
         </Container>
     </div>
 }
