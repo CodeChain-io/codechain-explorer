dc4e01f6716800033cbe21ee7ada45b0e56b6bfc
Gilyoung Kim <roadzero@kodebox.io>
2018-08-27 15:45:26 +0900

    Use relatice scheme for the http protocol

--- src/client/components/util/ImageLoader/ImageLoader.tsx
+++ src/client/components/util/ImageLoader/ImageLoader.tsx
@@ -17,7 +17,7 @@ export class ImageLoader extends React.Component<Props, State> {
         super(prop);
         let requestUrl;
         if (prop.url) {
-            requestUrl = prop.url;
+            requestUrl = prop.url.replace("http://", "//");
         } else {
             requestUrl = this.getDefaultImage()
         }
