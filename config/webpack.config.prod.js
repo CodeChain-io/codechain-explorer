4d983c3c116a6de6349b66c5bf72db7f99469745
Gilyoung <roadzero@kodebox.io>
2019-01-22 23:11:44 +0900

    Update the version of the webpack library

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -37,7 +37,7 @@ if (env.stringified["process.env"].NODE_ENV !== '"production"') {
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = "static/css/[name].[contenthash:8].css";
+const cssFilename = "static/css/[name].[md5:contenthash:hex:20].css";
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
