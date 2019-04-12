8b404f282863b13b98c23b9ea2188f83bbdabb12
JinGyeong Jeong <jjg@kodebox.io>
2019-04-12 14:50:10 +0900

    Revert "Remove useless RequestBlockNumber in the header"
    This reverts commit f058ba6c455416b36df7c13a3df21aebc6516352.

--- src/components/header/Header/Header.tsx
+++ src/components/header/Header/Header.tsx
@@ -20,6 +20,7 @@ import Search from "../Search/Search";
 
 import * as logo from "./img/logo-explorer.png";
 
+import { RequestBlockNumber } from "../../../request";
 import "./Header.scss";
 
 interface State {
@@ -38,6 +39,7 @@ class Header extends React.Component<{}, State> {
     public render() {
         return (
             <div className="header">
+                <RequestBlockNumber repeat={5000} onBlockNumber={this.handleRequestBlock} onError={this.handleError} />
                 <Navbar className="header-container" dark={true} expand="xl">
                     <Container fluid={true}>
                         <div className="header-title-container">
@@ -108,6 +110,13 @@ class Header extends React.Component<{}, State> {
             isOpen: !this.state.isOpen
         });
     }
+
+    private handleRequestBlock = () => {
+        // nothing
+    };
+    private handleError = (error: string) => {
+        console.log(error);
+    };
 }
 
 export default Header;
