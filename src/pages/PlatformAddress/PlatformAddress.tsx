74db8243f18c4e24c02b3922630fda3e381bbece
JinGyeong Jeong <jjg@kodebox.io>
2019-04-16 18:52:34 +0900

    Add a balance history component to the platform address page

--- src/pages/PlatformAddress/PlatformAddress.tsx
+++ src/pages/PlatformAddress/PlatformAddress.tsx
@@ -1,28 +1,38 @@
 import * as QRCode from "qrcode.react";
 import * as React from "react";
+import { connect, Dispatch } from "react-redux";
 import { match } from "react-router";
+import { Link } from "react-router-dom";
 import { Col, Container, Row } from "reactstrap";
-import { Error } from "../../components/error/Error/Error";
 
 import { BlockDoc, TransactionDoc } from "codechain-indexer-types";
 import { U256 } from "codechain-sdk/lib/core/classes";
+
 import BlockList from "../../components/block/BlockList/BlockList";
+import { Error } from "../../components/error/Error/Error";
 import AccountDetails from "../../components/platformAddress/AccountDetails/AccountDetails";
+import TransactionList from "../../components/transaction/TransactionList/TransactionList";
+import CopyButton from "../../components/util/CopyButton/CopyButton";
+import { ImageLoader } from "../../components/util/ImageLoader/ImageLoader";
 import {
     RequestPlatformAddressAccount,
     RequestTotalPlatformBlockCount,
     RequestTotalTransactionCount
 } from "../../request";
 import RequestPlatformAddressBlocks from "../../request/RequestPlatformAddressBlocks";
-
-import TransactionList from "../../components/transaction/TransactionList/TransactionList";
-import CopyButton from "../../components/util/CopyButton/CopyButton";
-import { ImageLoader } from "../../components/util/ImageLoader/ImageLoader";
 import RequestPlatformAddressTransactions from "../../request/RequestPlatformAddressTransactions";
+
+import { CommaNumberString } from "src/components/util/CommaNumberString/CommaNumberString";
+import DataTable from "src/components/util/DataTable/DataTable";
+import { apiRequest } from "src/request/ApiRequest";
 import "./PlatformAddress.scss";
 
-interface Props {
+interface OwnProps {
     match: match<{ address: string }>;
+}
+
+interface DispatchProps {
+    dispatch: Dispatch;
 }
 
 interface State {
@@ -41,11 +51,26 @@ interface State {
     totalBlockCount?: number;
     totalTransactionCount?: number;
     noMoreTransaction: boolean;
+
+    balanceChanges?: {
+        change: string;
+        blockNumber: number;
+        reason: "fee" | "author" | "stake" | "tx" | "initial_distribution";
+        transactionHash?: string;
+        transaction?: {
+            type: string;
+        };
+    }[];
+    balanceChangesCount?: number;
+    balanceChangesNextPage?: number;
 }
 
-class Address extends React.Component<Props, State> {
+type Props = OwnProps & DispatchProps;
+
+class PlatformAddress extends React.Component<Props, State> {
     private blockItemsPerPage = 6;
     private transactionsPerPage = 6;
+    private balanceChangesPerPage = 12;
     constructor(props: Props) {
         super(props);
         this.state = {
@@ -85,9 +110,17 @@ class Address extends React.Component<Props, State> {
                 noMoreBlock: false,
                 noMoreTransaction: false,
                 totalBlockCount: undefined,
-                totalTransactionCount: undefined
+                totalTransactionCount: undefined,
+                balanceChanges: undefined,
+                balanceChangesCount: undefined,
+                balanceChangesNextPage: undefined
             });
+            setTimeout(() => this.loadBalanceHistory());
         }
+    }
+
+    public componentDidMount() {
+        this.loadBalanceHistory();
     }
 
     public render() {
@@ -156,6 +189,7 @@ class Address extends React.Component<Props, State> {
                 <div className="mt-large">
                     <AccountDetails account={account} />
                 </div>
+                {this.renderBalanceChanges()}
                 {totalBlockCount == null && (
                     <RequestTotalPlatformBlockCount
                         address={address}
@@ -213,6 +247,149 @@ class Address extends React.Component<Props, State> {
             </Container>
         );
     }
+
+    private renderBalanceChanges = () => {
+        const { balanceChanges, balanceChangesCount, balanceChangesNextPage } = this.state;
+
+        if (balanceChanges == null) {
+            return <></>;
+        }
+        return (
+            <div className="mt-large">
+                <Row>
+                    <Col>
+                        <div className="d-flex justify-content-between align-items-end">
+                            <h2>Balance Log</h2>
+                            <span>Total {balanceChangesCount} changes</span>
+                        </div>
+                        <hr className="heading-hr" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <DataTable>
+                            <thead>
+                                <tr>
+                                    <th style={{ width: "10%" }}>Block</th>
+                                    <th style={{ width: "20%" }}>Reason</th>
+                                    <th style={{ width: "35%" }} className="text-right">
+                                        Spend
+                                    </th>
+                                    <th style={{ width: "35%" }} className="text-right">
+                                        Receive
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {balanceChanges.map(
+                                    ({ blockNumber, change, reason, transactionHash, transaction }, i) => (
+                                        <tr key={i}>
+                                            <td>{blockNumber}</td>
+                                            <td>
+                                                {this.renderBalanceChangeReason(
+                                                    reason,
+                                                    transactionHash,
+                                                    transaction && transaction.type
+                                                )}
+                                            </td>
+                                            <td className="text-right">
+                                                {change.startsWith("-") ? <CommaNumberString text={change} /> : ""}
+                                            </td>
+                                            <td className="text-right">
+                                                {change.startsWith("-") ? (
+                                                    ""
+                                                ) : (
+                                                    <>
+                                                        +<CommaNumberString text={change} />
+                                                    </>
+                                                )}
+                                            </td>
+                                        </tr>
+                                    )
+                                )}
+                            </tbody>
+                        </DataTable>
+                    </Col>
+                </Row>
+                {(balanceChangesNextPage! - 1) * this.balanceChangesPerPage < balanceChangesCount! && (
+                    <Row>
+                        <Col>
+                            <div className="mt-small">
+                                <button className="btn btn-primary w-100" onClick={this.loadBalanceHistory}>
+                                    Load changes
+                                </button>
+                            </div>
+                        </Col>
+                    </Row>
+                )}
+            </div>
+        );
+    };
+
+    private renderBalanceChangeReason = (
+        reason: "fee" | "author" | "stake" | "tx" | "initial_distribution",
+        transactionHash?: string,
+        transactionType?: string
+    ) => {
+        const type = transactionType && transactionType.charAt(0).toUpperCase() + transactionType.slice(1);
+        switch (reason) {
+            case "fee":
+                return <Link to={`/tx/0x${transactionHash}`}>{type} (fee)</Link>;
+            case "author":
+                return <>Block author reward</>;
+            case "stake":
+                return <>Stake reward</>;
+            case "tx":
+                return <Link to={`/tx/0x${transactionHash}`}>{type}</Link>;
+            case "initial_distribution":
+                return <>Genesis account</>;
+        }
+    };
+
+    private loadBalanceHistory = () => {
+        const {
+            match: {
+                params: { address }
+            },
+            dispatch
+        } = this.props;
+        const { balanceChangesNextPage } = this.state;
+        if (balanceChangesNextPage == null) {
+            Promise.all([
+                apiRequest({
+                    path: `account/${address}/balance-history?page=1&itemsPerPage=${this.balanceChangesPerPage}`,
+                    dispatch,
+                    showProgressBar: true
+                }),
+                apiRequest({ path: `account/${address}/balance-history/count`, dispatch, showProgressBar: true })
+            ])
+                .then(([response, count]) => {
+                    // FIXME: Remove any
+                    this.setState({
+                        balanceChanges: response as any,
+                        balanceChangesCount: count as number,
+                        balanceChangesNextPage: 2
+                    });
+                })
+                .catch(this.onError);
+        } else {
+            apiRequest({
+                path: `account/${address}/balance-history?page=${balanceChangesNextPage}&itemsPerPage=${
+                    this.balanceChangesPerPage
+                }`,
+                dispatch,
+                showProgressBar: true
+            })
+                .then(response => {
+                    this.setState({
+                        balanceChanges: [...this.state.balanceChanges!, ...(response as any)],
+                        balanceChangesNextPage: balanceChangesNextPage + 1
+                    });
+                })
+                .catch(this.onError);
+        }
+    };
+
     private onBlocks = (blocks: BlockDoc[]) => {
         if (blocks.length < this.blockItemsPerPage) {
             this.setState({ noMoreBlock: true });
@@ -266,4 +443,4 @@ class Address extends React.Component<Props, State> {
     };
 }
 
-export default Address;
+export default connect()(PlatformAddress);
