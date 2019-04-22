623504467ef0d2a44b6d1f81c0356541b2fa0eed
Seongjae Choi <sjchoi@kodebox.io>
2019-04-22 21:46:01 +0900

    Add asset owners table in the asset page

---
+++ src/components/asset/AssetOwners/AssetOwners.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+
+import { AggsUTXODoc } from "codechain-indexer-types";
+import { Link } from "react-router-dom";
+import { Col, Row } from "reactstrap";
+import { CommaNumberString } from "src/components/util/CommaNumberString/CommaNumberString";
+import DataTable from "src/components/util/DataTable/DataTable";
+import { ImageLoader } from "src/components/util/ImageLoader/ImageLoader";
+import "./AssetOwners.scss";
+
+interface OwnProps {
+    aggsUTXO: AggsUTXODoc[];
+}
+
+const AssetOwners = (prop: OwnProps) => {
+    return (
+        <div className="asset-details">
+            <Row>
+                <Col>
+                    <h2>Asset Owners</h2>
+                    <hr className="heading-hr" />
+                </Col>
+            </Row>
+            <DataTable>
+                <thead>
+                    <tr>
+                        <th style={{ width: "50%" }}>Address</th>
+                        <th style={{ width: "50%" }} className="text-right">
+                            Quantity
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {prop.aggsUTXO.map((item: AggsUTXODoc) => {
+                        return (
+                            <tr key={`raw-${item.address}`} className="animated fadeIn">
+                                <td>
+                                    <ImageLoader className="mr-2" size={18} data={item.address} isAssetImage={true} />
+                                    <Link to={`/addr-asset/${item.address}`}>{item.address}</Link>
+                                </td>
+                                <td className="text-right">
+                                    <CommaNumberString text={item.totalAssetQuantity} />
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </DataTable>
+        </div>
+    );
+};
+
+export default AssetOwners;
